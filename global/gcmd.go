58e0a49e4547717f5e485e43cdc55c842d22fe18
camsiabor <kkidthelordzlight@163.com>
2018-12-17 20:41:56 +0800

    compress setting

--- global/gcmd.go
+++ global/gcmd.go
@@ -2,117 +2,126 @@ package global
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
 type Cmd struct {
-	ID       string;
-	Flag     int;
-	SFlag    string;
-	Service  string;
-	Function string;
-	Timeout  time.Duration;
-	Data     map[string]interface{};
-	RetVal   interface{};
-	RetErr   error;
-	Ret 	 * Cmd;
-	retChan  chan * Cmd;
-	retChanLen int;
+	ID         string
+	Flag       int
+	SFlag      string
+	Service    string
+	Function   string
+	Timeout    time.Duration
+	RetVal     interface{}
+	RetErr     error
+	Ret        *Cmd
+	retChan    chan *Cmd
+	retChanLen int
+	lock       sync.RWMutex
+	data       map[string]interface{}
 }
 
 type CmdHandler interface {
-	HandleCmd(cmd * Cmd) (reply * Cmd, handled bool, err error);
+	HandleCmd(cmd *Cmd) (reply *Cmd, handled bool, err error)
 }
 
-
-func (o * Cmd) GetServFunc() string {
-	return o.Service + "." + o.Function;
+func (o *Cmd) GetServFunc() string {
+	return o.Service + "." + o.Function
 }
 
-func (o * Cmd) GetData(key string) interface{} {
-	if (o.Data == nil) {
-		return nil;
-	}
-	return o.Data[key];
-}
-
-
-func (o * Cmd) SetData(key string, val interface{}) {
-	if (o.Data == nil) {
-		o.Data = make(map[string]interface{});
-	}
-	o.Data[key] = val;
-}
-
-func (o * Cmd) InitRetChannel(retChanLen int) {
-	if (o.retChan == nil) {
-		o.retChanLen = retChanLen;
-		if (o.retChanLen <= 0) {
-			o.retChanLen = 0;
+func (o *Cmd) Data() map[string]interface{} {
+	if o.data == nil {
+		o.lock.Lock()
+		if o.data == nil {
+			o.data = make(map[string]interface{})
 		}
-		o.retChan = make(chan * Cmd);
+		o.lock.Unlock()
 	}
+	return o.data
 }
 
-func (o * Cmd) DestroyRetChannel() {
-	if (o.retChan == nil) {
-		return;
+func (o *Cmd) GetData(key string) interface{} {
+	if o.data == nil {
+		return nil
 	}
-	close(o.retChan);
-	o.retChan = nil;
-	o.retChanLen = -1;
+	o.lock.RLock()
+	defer o.lock.RUnlock()
+	return o.data[key]
 }
 
-func (o * Cmd) Wait(timeout time.Duration) (reply * Cmd, err error) {
-	
-	if (timeout == 0) {
-		return nil, nil;
+func (o *Cmd) SetData(key string, val interface{}) {
+	o.lock.Lock()
+	defer o.lock.Unlock()
+	if o.data == nil {
+		o.data = make(map[string]interface{})
 	}
+	o.data[key] = val
+}
 
-	o.InitRetChannel(2);
-
-	o.Timeout = timeout;
-
-	var rok bool;
-	if (timeout > 0) {
-		var timeout = time.After(timeout);
-		select {
-		case reply, rok = <- o.retChan:
-			if (!rok) {
-				err = errors.New("return channel close");
-			}
-		case <-timeout:
-			err = errors.New("timeout");
+func (o *Cmd) InitRetChannel(retChanLen int) {
+	if o.retChan == nil {
+		o.retChanLen = retChanLen
+		if o.retChanLen <= 0 {
+			o.retChanLen = 0
 		}
-	} else if (timeout < 0) {
+		o.retChan = make(chan *Cmd)
+	}
+}
+
+func (o *Cmd) DestroyRetChannel() {
+	if o.retChan == nil {
+		return
+	}
+	close(o.retChan)
+	o.retChan = nil
+	o.retChanLen = -1
+}
+
+func (o *Cmd) Wait(timeout time.Duration) (reply *Cmd, err error) {
+
+	if timeout == 0 {
+		return nil, nil
+	}
+
+	o.InitRetChannel(2)
+
+	o.Timeout = timeout
+
+	var rok bool
+	if timeout > 0 {
+		var timeout = time.After(timeout)
 		select {
 		case reply, rok = <-o.retChan:
-			if (!rok) {
-				err = errors.New("return channel close");
+			if !rok {
+				err = errors.New("return channel close")
+			}
+		case <-timeout:
+			err = errors.New("timeout")
+		}
+	} else if timeout < 0 {
+		select {
+		case reply, rok = <-o.retChan:
+			if !rok {
+				err = errors.New("return channel close")
 			}
 		}
 	}
-	return reply, err;
+	return reply, err
 }
 
-func (o * Cmd) Reply(reply * Cmd) {
-	if (reply == nil) {
-		reply = o;
+func (o *Cmd) Reply(reply *Cmd) {
+	if reply == nil {
+		reply = o
 	}
-	o.Ret = reply;
-	if (o.retChan != nil) {
-		o.retChan <- reply;
+	o.Ret = reply
+	if o.retChan != nil {
+		o.retChan <- reply
 	}
 }
 
-func (o * Cmd) ReplySelf(retval interface{}, err error) {
-	o.RetErr = err;
-	o.RetVal = retval;
-	o.Reply(o);
+func (o *Cmd) ReplySelf(retval interface{}, err error) {
+	o.RetErr = err
+	o.RetVal = retval
+	o.Reply(o)
 }
-
-
-
-
-
-
