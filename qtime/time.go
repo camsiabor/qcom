3ec88238925a49b11d17ecbd7d53d44661cd8ee3
camsiabor <kkidthelordzlight@163.com>
2019-01-09 20:08:59 +0800

    stock week month sync

--- qtime/time.go
+++ qtime/time.go
@@ -7,137 +7,149 @@ import (
 	"time"
 )
 
-func Date2Int(t * time.Time) (int) {
-	if (t == nil) {
-		var n = time.Now();
-		t = &n;
+func Date2Int(t *time.Time) int {
+	if t == nil {
+		var n = time.Now()
+		t = &n
 	}
-	var y, m, d = t.Date();
-	return int(y * 10000) + int(m * 100) + d;
+	var y, m, d = t.Date()
+	return int(y*10000) + int(m*100) + d
 }
 
-func Time2Int64(t * time.Time) (int64) {
-	if (t == nil) {
-		var n = time.Now();
-		t = &n;
+func Time2Int64(t *time.Time) int64 {
+	if t == nil {
+		var n = time.Now()
+		t = &n
 	}
-	var y, m, d = t.Date();
-	var hour, min, sec = t.Clock();
-	var date = int64(y * 10000000000) + int64(m * 100000000) + int64(d * 1000000);
-	var clock int64 = int64(hour * 10000) + int64(min * 100) + int64(sec);
-	return date + clock;
+	var y, m, d = t.Date()
+	var hour, min, sec = t.Clock()
+	var date = int64(y*10000000000) + int64(m*100000000) + int64(d*1000000)
+	var clock int64 = int64(hour*10000) + int64(min*100) + int64(sec)
+	return date + clock
 }
 
-func ParseTime(s string) (* time.Time, error) {
-	var err error;
-	var t * time.Time;
-	if (strings.Contains(s, "-")) {
-		if (strings.Contains(s, ":")) {
-			if (len(s) > 8) {
-				*t, err = time.Parse("2006-01-02 15:04:05", s);
+func ParseTime(s string) (*time.Time, error) {
+	var err error
+	var t *time.Time
+	if strings.Contains(s, "-") {
+		if strings.Contains(s, ":") {
+			if len(s) > 8 {
+				*t, err = time.Parse("2006-01-02 15:04:05", s)
 			} else {
-				*t, err = time.Parse("15:04:05", s);
+				*t, err = time.Parse("15:04:05", s)
 			}
 		} else {
-			*t, err = time.Parse("2006-01-02", s);
+			*t, err = time.Parse("2006-01-02", s)
 		}
-	} else if (strings.Contains(s, "/")) {
-		if (strings.Contains(s, ":")) {
-			if (len(s) > 8) {
-				*t, err = time.Parse("2006/01/02 15:04:05", s);
+	} else if strings.Contains(s, "/") {
+		if strings.Contains(s, ":") {
+			if len(s) > 8 {
+				*t, err = time.Parse("2006/01/02 15:04:05", s)
 			} else {
-				*t, err = time.Parse("15:04:05", s);
+				*t, err = time.Parse("15:04:05", s)
 			}
 		} else {
-			*t, err = time.Parse("2006/01/02", s);
+			*t, err = time.Parse("2006/01/02", s)
 		}
 	} else {
-		if (len(s) > 8) {
-			*t, err = time.Parse("20060102 150405", s);
+		if len(s) > 8 {
+			*t, err = time.Parse("20060102 150405", s)
 		} else {
-			*t, err = time.Parse("20060102", s);
+			*t, err = time.Parse("20060102", s)
 		}
 	}
-	if (err != nil) {
-		return nil, err;
+	if err != nil {
+		return nil, err
 	}
-	return t, err;
+	return t, err
 }
 
-func TimeInterval(t1 * time.Time, t2 * time.Time, duration time.Duration) float64 {
-	if (t2 == nil) {
-		var n = time.Now();
-		t2 = &n;
+func TimeInterval(t1 *time.Time, t2 *time.Time, duration time.Duration) float64 {
+	if t2 == nil {
+		var n = time.Now()
+		t2 = &n
 	}
 	var interval = float64(t1.UnixNano() - t2.UnixNano())
-	var d = float64(duration);
-	return math.Ceil(interval / d);
+	var d = float64(duration)
+	return math.Ceil(interval / d)
 }
 
-func YYYY_MM_dd_HH_mm_ss(t * time.Time) (string) {
-	if (t == nil) {
-		var s = time.Now();
-		t = &s;
+func YYYY_MM_dd_HH_mm_ss(t *time.Time) string {
+	if t == nil {
+		var s = time.Now()
+		t = &s
 	}
-	return t.Format("2006-01-02 15:04:05");
+	return t.Format("2006-01-02 15:04:05")
 }
 
-func YYYY_MM_dd(t * time.Time) (string) {
-	if (t == nil) {
-		var s = time.Now();
-		t = &s;
+func YYYY_MM_dd(t *time.Time) string {
+	if t == nil {
+		var s = time.Now()
+		t = &s
 	}
-	return t.Format("2006-01-02");
+	return t.Format("2006-01-02")
 }
 
-func GetTimeFormatIntervalArray(from * time.Time, to * time.Time, layout string, exclude ... time.Weekday) ([]string, error) {
+func GetTimeFormatIntervalArray(from *time.Time, to *time.Time, layout string, lastday bool, exclude ...time.Weekday) ([]string, error) {
 
-
-	if (to == nil) {
-		var s = time.Now();
-		to = &s;
+	if to == nil {
+		var s = time.Now()
+		to = &s
 	}
 
-	var i = 0;
-	var time_array []string = make([]string, 256);
-	time_array[i] = from.Format(layout);
-	i = i + 1;
+	var i = 0
+	var time_array []string = make([]string, 256)
+	time_array[i] = from.Format(layout)
+	i = i + 1
 
 	var fromnum = Date2Int(from)
-	var tonum = Date2Int(to);
-	if (fromnum > tonum) {
-		return nil, fmt.Errorf("from time > to time");
+	var tonum = Date2Int(to)
+	if fromnum > tonum {
+		return nil, fmt.Errorf("from time > to time")
 	}
 
-	var excludearray []int;
-	var doexclude = exclude != nil && len(exclude) > 0;
-	if (doexclude) {
-		excludearray = make([]int, 7);
+	var excludearray []int
+	var doexclude = exclude != nil && len(exclude) > 0
+	if doexclude {
+		excludearray = make([]int, 7)
 		for i := 0; i < len(exclude); i++ {
-			var one = exclude[i];
-			excludearray[one] = 1;
+			var one = exclude[i]
+			excludearray[one] = 1
 		}
 	}
 
-	if (fromnum < tonum) {
-		var middle = from.AddDate(0, 0, 0);
+	if fromnum < tonum {
+		var middle = from.AddDate(0, 0, 0)
 		for {
-			middle = middle.AddDate(0, 0, 1);
-			var middlenum = Date2Int(&middle);
-			if (middlenum > tonum) {
-				break;
+			middle = middle.AddDate(0, 0, 1)
+			var middlenum = Date2Int(&middle)
+			if middlenum > tonum {
+				break
 			}
 
-			if (doexclude) {
-				if (excludearray[middle.Weekday()] != 0) {
-					continue;
+			if doexclude {
+				if excludearray[middle.Weekday()] != 0 {
+					continue
 				}
 			}
 
-			time_array[i] = middle.Format(layout);
-			i = i + 1;
+			if lastday {
+				var day = middle.Day()
+				if day >= 28 {
+					var next = middle.AddDate(0, 0, 1)
+					if next.Month() == middle.Month() {
+						continue
+					}
+				} else {
+					continue
+				}
+			}
+
+			time_array[i] = middle.Format(layout)
+			i = i + 1
+
 		}
 	}
 
-	return time_array[:i], nil;
+	return time_array[:i], nil
 }
