1d68e39f0108235d8de868617cbeb601d2e44d7e
calix.lam <kkidthelordzlight@163.com>
2019-07-12 11:10:05 +0800

    zk modification

--- util/channel.go
+++ util/channel.go
@@ -1,15 +1,45 @@
 package util
 
-import "time"
+import (
+	"reflect"
+	"time"
+)
 
-func Timeout(ch chan interface{}, timeout time.Duration) (retValue interface{}, isClosed bool, isTimeout bool) {
+func Timeout(channel interface{}, timeout time.Duration) (chosen int, recv reflect.Value, recvok bool) {
+
+	var selectCases = make([]reflect.SelectCase, 2)
+
 	var timer = time.After(timeout)
-	var ok bool
-	var ret interface{}
-	select {
-	case ret, ok = <-ch:
-		return ret, ok, false
-	case <-timer:
-		return nil, true, true
+	selectCases[0].Dir = reflect.SelectRecv
+	selectCases[0].Chan = reflect.ValueOf(channel)
+	selectCases[1].Dir = reflect.SelectRecv
+	selectCases[1].Chan = reflect.ValueOf(timer)
+
+	chosen, recv, recvok = reflect.Select(selectCases)
+	if chosen == 1 {
+		chosen = -1
 	}
+	return chosen, recv, recvok
+}
+
+func Timeouts(channels []interface{}, timeout time.Duration) (chosen int, recv reflect.Value, recvok bool) {
+	var n = len(channels)
+	var selectCases = make([]reflect.SelectCase, n+1)
+	for i := 0; i < n; i++ {
+		var ch = channels[i]
+		if ch == nil {
+			panic("invalid parameters, channel cannot be null")
+		}
+		selectCases[i].Dir = reflect.SelectRecv
+		selectCases[i].Chan = reflect.ValueOf(channels[i])
+	}
+	var timer = time.After(timeout)
+	selectCases[n].Dir = reflect.SelectRecv
+	selectCases[n].Chan = reflect.ValueOf(timer)
+
+	chosen, recv, recvok = reflect.Select(selectCases)
+	if chosen == n {
+		chosen = -1
+	}
+	return chosen, recv, recvok
 }
